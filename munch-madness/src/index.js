47b9787105037772243b5e7842c86de0d324523b
acdoyle630@gmail.com <acdoyle630@gmail.com>
2019-10-02 20:26:14 -0500

    commit

--- munch-madness/src/index.js
+++ munch-madness/src/index.js
@@ -16,7 +16,7 @@ import ChooseSettings from './scenes/choose-settings/choose-settings'
 import ChooseCategories from './scenes/choose-categories/choose-categories'
 import Bracket from './scenes/bracket/bracket'
 import './index.css'
-
+ 
 const store = createStore(
   reducers,
   //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
