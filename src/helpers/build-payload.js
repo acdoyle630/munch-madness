91dc117c30521bd86c06e2b0f03d6fb7a965cf0c
acdoyle630@gmail.com <acdoyle630@gmail.com>
2020-04-18 10:22:27 -0600

    fix price selector

--- src/helpers/build-payload.js
+++ src/helpers/build-payload.js
@@ -1,37 +1,39 @@
-
-export const buildPayload = ( 
-    category,
-    selectedPrice,
-    selectedDistance,
-    attributes,
-    zip
-    ) => {
-        return {
-            location: zip,
-            term: category,
-            price: allowedPrices(selectedPrice),
-            open_now: attributes.open_now,
-            radius: selectedDistance || 40000,
-            attributes: allowedAttributes(attributes)
-        }
-}
+export const buildPayload = (
+  category,
+  selectedPrice,
+  selectedDistance,
+  attributes,
+  zip
+) => {
+  return {
+    location: zip,
+    term: category,
+    price: allowedPrices(selectedPrice),
+    open_now: attributes.open_now,
+    radius: selectedDistance || 40000,
+    attributes: allowedAttributes(attributes),
+  };
+};
 
 const allowedPrices = (selectedPrice) => {
-    let prices = []
-        Object.keys(selectedPrice).forEach((price) => {
-            if( selectedPrice[price] ) {
-                prices.push(Number(price) + 1).toString()
-            }
-        })
-        return prices.length > 0
-            ? prices.join(',')
-            : '1,2,3,4'
-}
+  if (selectedPrice === null) {
+    return "1,2,3,4";
+  }
+  let prices = [];
+  let i = 1;
+  while (prices.length <= selectedPrice) {
+    prices.push(i.toString());
+    i++;
+  }
+  return prices.length > 0 && prices.join(",");
+};
 
 const allowedAttributes = (attributes) => {
-    return Object.keys(attributes).filter((attribute) => {
-        if(attributes[attribute]){
-            return attribute
-        }
-    }).join(',')
-}
+  return Object.keys(attributes)
+    .filter((attribute) => {
+      if (attributes[attribute]) {
+        return attribute;
+      }
+    })
+    .join(",");
+};
--- src/services/yelp.js
+++ src/services/yelp.js
@@ -1,20 +1,23 @@
-import { get } from 'axios'
-import keys from '../private/keys.json'
+import { get } from "axios";
+import keys from "../private/keys.json";
 
-export const search = ( payload ) => {
-    const baseUrl = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?'
-    return get(
-        `${baseUrl}location=${payload.location}&term=${payload.term}&price=${payload.price}&open_now=${payload.open_now}&radius=${payload.radius}&attributes=${payload.attributes}`,
-        {
-            headers: {
-                'Authorization': "bearer " + keys.yelp,
-                "Content-Type": "application/json",
-                'Access-Control-Allow-Origin': '*',
-            }
-        }
-    ).then((response) => {
-        return response.data.businesses
-    }).catch((e) => {
-        console.log(e)
+export const search = (payload) => {
+  const baseUrl =
+    "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?";
+  return get(
+    `${baseUrl}location=${payload.location}&term=${payload.term}&price=${payload.price}&open_now=${payload.open_now}&radius=${payload.radius}&attributes=${payload.attributes}`,
+    {
+      headers: {
+        Authorization: "bearer " + keys.yelp,
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    }
+  )
+    .then((response) => {
+      return response.data.businesses;
     })
-}
+    .catch((e) => {
+      console.log(e);
+    });
+};
